7300e940ccf58a06ed8de4d0d4b2cc0126d25180
everctba <everson777@gmail.com>
2024-07-22 21:40:48 -0300

    Update Dijkstra-Algorithm.py

--- Dijkstra-Algorithm.py
+++ Dijkstra-Algorithm.py
@@ -3,10 +3,35 @@ import pygame
 import sys
 
 WINDOW_WIDTH = 500
-WINDOW_HEIGTH = 500
+WINDOW_HEIGHT = 500
 
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGTH))
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Dijkstra Algorithm do Youtube tutorial: https://www.youtube.com/watch?v=QNpUN8gBeLY&ab_channel=MaxonTech")
+
+columns = 25
+rows = 25
+
+box_width = WINDOW_WIDTH // columns
+box_height = WINDOW_HEIGHT // rows
+
+grid = []
+
+class Box:
+   def __init__(self, i, j):
+      self.x = i
+      self.j = j
+      
+      def draw(self, win, color):
+         pygame.draw.rect(win, color, (self.x * box_width, self.y * box_height, box_width - 2, box_height - 2))
+
+# Create Grid
+for i in range(columns):
+   arr = []
+   for j in range(rows):
+      # print("j = ", j)
+      arr.append(Box(i,j))
+   grid.append(arr)
+
 
 def main():
    while True:
@@ -17,6 +42,11 @@ def main():
             sys.exit()
             
       window.fill((0,0,0))
+      
+      for i in range(columns):
+         for j in range(rows):
+            box = grid[i][j]
+            box.draw(window, (50, 50, 50))
       
       pygame.display.flip()
       
